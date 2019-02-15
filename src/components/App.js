f9a25d0ebeddfac20813574b1f7d089ff52ad6d5
Mauricio Feldman-Abe <mauabe@gmx.com>
2019-02-15 13:30:17 -0800

    App.js setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,35 @@
 import React, { Component} from'react';
 
+const API_ADDRESS = 
 
 class App extends Component{
-  
+  state = { artistQuery: ''};
+
+  updateArtistQuery = (event) => {
+    console.log('event.target.value', event.target.value);
+    this.setState({artistQuery: event.target.value})
+  }
+
+  handleKeyPress = event => {
+    if(event.key === 'Enter'){
+      this.searchArtist();
+    }
+  }
+
+  searchArtist = () => {
+    console.log('this.state', this.state);
+  }
+
   render(){
     return(
-      <div> React App
-
+      <div> 
+        <h2>mmavens</h2>
+        <input 
+          onChange={this.updateArtistQuery}
+          placeholder='Search for and Artist' 
+          onKeyPress={this.handleKeyPress}
+          />
+        <button onClick={this.searchArtist}> Search</button>
       </div>
     )
   }
