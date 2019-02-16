2975abdac525ec543b763a57216e2c6272fe7b1a
Mauricio Feldman-Abe <mauabe@gmx.com>
2019-02-15 16:32:31 -0800

    fetched data using Axios, instead of fetch

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,25 @@
 import React, { Component} from'react';
+import axios from 'axios'
 
-const API_ADDRESS = 
+const API_ADDRESS = 'https://spotify-api-wrapper.appspot.com/'
+const URL = 'https://spotify-api-wrapper.appspot.com/'
+
+// let result = () => {
+//   axios.get(URL)
+//   .then((response) => {
+//     console.log(response);
+//     return response;
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
+// }
+
 
 class App extends Component{
-  state = { artistQuery: ''};
+  state = { artistQuery: '', artist: null, tracks: []};
 
   updateArtistQuery = (event) => {
-    console.log('event.target.value', event.target.value);
     this.setState({artistQuery: event.target.value})
   }
 
@@ -16,11 +29,42 @@ class App extends Component{
     }
   }
 
+  //using fetch
+  // searchArtist = () => {
+  //   console.log('this.state', this.state);
+  //   fetch(`${API_ADDRESS}/artist/${this.state.artistQuery}`)
+  //   .then(response => response.json())
+  //   .then(json => {
+  //     console.log('json', json)
+  //   })
+  //   .catch(error => {
+  //     console.log('error', error)
+  //   })
+  // }
+
+  //using axios, baby!
   searchArtist = () => {
-    console.log('this.state', this.state);
+    axios.get(`${URL}/artist/${this.state.artistQuery}`)
+    .then((response) => {
+      if (response.data.artists.items.length > 0){
+        const artist = response.data.artists.items[0];
+        const artistId = response.data.artists.items[0].id;
+        this.setState({ artist, artistId })
+
+        axios.get(`${URL}/artist/${artistId}/top-tracks`)
+          .then(tracksObj => {
+            this.setState({ tracks: tracksObj.data.tracks })
+            // console.log('tracks tracksObj', tracksObj.data.tracks)
+          })
+          .catch(error => alert(error.message))
+      }
+    }) 
+    .catch(error => alert(error.message))
   }
+  
 
   render(){
+    console.log('this.state at render', this.state);
     return(
       <div> 
         <h2>mmavens</h2>
